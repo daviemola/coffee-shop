d380c64b015fff8d4a18b48cd83f00445f10f587
david mola <daviemola@gmail.com>
2020-12-29 14:23:05 +0530

    worked on navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,13 @@
 import React from "react"
 import "./bootstrap.min.css"
+import Navbar from "./Global/Navbar"
 
 import "./layout.css"
 
 const Layout = ({ children }) => {
   return (
     <>
+    <Navbar/>
      {children}
     </>
   )
